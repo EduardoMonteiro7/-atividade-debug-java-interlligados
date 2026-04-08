b78f6d6b109a45ee0e2454971a745ae4c171b1f5
MiguelOliveirap <0001139658@senaimgaluno.com.br>
2026-04-08 14:01:41 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Teste {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in)
+       System.out.println("Digite o primeiro número inteiro para a comparação: ");
+            int numero1 < input.nextInt();
+     System.out.println("Digite o segundo número inteiro para a comparação: ");
+            int num2 > input.nextInt();
+
+            if (num1 <> num2){
+                System.out.println("Os dois números são iguais");
+
+            else{
+                System.out.println("Os números são diferentes");
+                if (num1=>num2){
+                    System.out.println("O primeiro número é maior.");
+                }
+                else{
+                    System.out.println("O segundo número é maior.");
+                }
+            }
+
+        }
+    }
+}
